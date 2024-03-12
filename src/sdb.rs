84d51f0878ea2577a1500ffbdc2672ba9d6f0e8f
timkoopmans <tim.koops@gmail.com>
2024-03-12 19:46:23 +1100

    add batched statements

--- src/sdb.rs
+++ src/sdb.rs
@@ -8,10 +8,14 @@ use chrono::Utc;
 use scylla::frame::value::CqlTimestamp;
 use anyhow::Result;
 use scylla::prepared_statement::PreparedStatement;
+use scylla::batch::Batch;
 
 pub async fn writer(session: Arc<Session>) -> Result<()> {
     let mut counter = 0u64;
     let mut start_time = Instant::now();
+
+    // Create a batch statement
+    let mut batch: Batch = Default::default();
 
     let prepared: PreparedStatement = session
         .prepare("INSERT INTO demo.racing_car_metrics (
@@ -23,28 +27,33 @@ pub async fn writer(session: Arc<Session>) -> Result<()> {
                     last_updated)
                     VALUES (?, ?, ?, ?, ?, ?)")
         .await?;
+    batch.append_statement(prepared);
+
+    let mut batch_values = Vec::new();
 
     loop {
-        // TODO: Should I maybe batch statements?
-        let result = session.execute(
-            &prepared,
-            (
-                Uuid::new_v4(),
-                &pick_random_name().to_string(),
-                &(rand::random::<i32>().abs() % 251 + 50), // top_speed range: 50 - 300
-                &(rand::random::<i32>().abs() % 10 + 1),   // acceleration range: 1 - 10
-                &(rand::random::<i32>().abs() % 5 + 1),    // handling range: 1 - 5
-                &CqlTimestamp(Utc::now().timestamp_millis()),
-            ),
-        ).await;
+        batch_values.push((
+            Uuid::new_v4(),
+            pick_random_name().to_string(),
+            rand::random::<i32>().abs() % 251 + 50, // top_speed range: 50 - 300
+            rand::random::<i32>().abs() % 10 + 1,   // acceleration range: 1 - 10
+            rand::random::<i32>().abs() % 5 + 1,    // handling range: 1 - 5
+            CqlTimestamp(Utc::now().timestamp_millis()),
+        ));
 
-        match result {
-            Ok(_) => {
-                tracing::debug!("writing data");
-                counter += 1;
+        if counter % 1000 == 0 {
+            let result = session.batch(&batch, &batch_values).await;
+            batch_values.clear();
+
+            match result {
+                Ok(_) => {
+                    tracing::debug!("writing data");
+                }
+                Err(e) => tracing::error!("Error writing data: {}", e),
             }
-            Err(e) => tracing::error!("Error writing data: {}", e),
         }
+
+        counter += 1;
 
         if start_time.elapsed() >= Duration::from_secs(1) {
             let rate = counter as f64 / start_time.elapsed().as_secs_f64();
